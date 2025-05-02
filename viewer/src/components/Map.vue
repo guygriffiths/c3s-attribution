2c7b769b22089a52128458de90584b9e8b3a3971
Guy Griffiths <guy.griffiths@reading.ac.uk>
2025-05-02 11:28:20 +0100

    Added viewer skeleton ready to turn into useful tool for debugging

---
+++ viewer/src/components/Map.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import 'leaflet/dist/leaflet.css'
+import { onMounted, ref, Ref } from 'vue'
+import {
+	LMap,
+	LTileLayer,
+	LControlScale,
+	LControlZoom,
+} from '@vue-leaflet/vue-leaflet'
+import { LatLng, Point } from 'leaflet'
+
+const mapOptions = {
+	zoomControl: false,
+	zoomSnap: 1,
+	zoomDelta: 1,
+	wheelPxPerZoomLevel: 240,
+}
+const centerPoint: Ref<Point> = ref(new LatLng(51, 0) as unknown as Point)
+const zoom = ref(10)
+
+const bgLayer = {
+	name: 'Stadia OSM Bright',
+	url: 'https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png',
+	attribution:
+		'&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+}
+</script>
+
+<template>
+	<div class="map">
+		<LMap
+			ref="map"
+			v-model:zoom="zoom"
+			:center="centerPoint"
+			:max-zoom="12"
+			:min-zoom="4"
+			:options="mapOptions"
+			style="z-index: 1"
+			:zoom-animation="true"
+			@ready=""
+		>
+			<LTileLayer
+				:url="bgLayer.url"
+				:attribution="bgLayer.attribution"
+				layer-type="base"
+				:zIndex="1"
+			></LTileLayer>
+			<LControlScale
+				:max-width="200"
+				:metric="true"
+				:imperial="false"
+				position="bottomright"
+				class="map-scale"
+			></LControlScale>
+			<LControlZoom></LControlZoom>
+		</LMap>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/styles/scssVars.scss' as *;
+
+.map {
+	width: 100%;
+	height: 100%;
+
+	:deep(.leaflet-control-zoom),
+	:deep(.leaflet-control-zoom-out),
+	:deep(.leaflet-control-zoom-in) {
+		background-color: $bg;
+		border-color: $bgContrast;
+	}
+}
+</style>
