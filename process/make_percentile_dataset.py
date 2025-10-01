23fffacbf2e5eb57a6e6d002ae7353a90cc5d051
Guy <guy.griffiths@reading.ac.uk>
2025-10-01 17:18:40 +0100

    Minor tweaks

--- process/make_percentile_dataset.py
+++ process/make_percentile_dataset.py
@@ -2,14 +2,15 @@
 
 import xarray as xr
 stats = ['max', 'mean']
-stat = 'min'
 percs = [0.98, 0.99, 0.995, 0.95]
+percs = [0.01, 0.02, 0.05, 0.005]
 years = range(1991,2021)
-files = [f"/data/{stat}/era5_daily_{stat}_temperature_{year}.nc" for year in years]
-ds = xr.open_mfdataset(files, combine="by_coords")
-t2m = ds["t2m"]
 
-for q in percs:
+for stat in stats:
+  files = [f"/data/{stat}/era5_daily_{stat}_temperature_{year}.nc" for year in years]
+  ds = xr.open_mfdataset(files, combine="by_coords")
+  t2m = ds["t2m"]
+  for q in percs:
    t2m_thresh = t2m.quantile(q, dim="valid_time")
 
    t2m_thresh.compute().to_netcdf(f"/data/era5_daily_{stat}_temperature_{q*100}pc_1991-2020.nc")
